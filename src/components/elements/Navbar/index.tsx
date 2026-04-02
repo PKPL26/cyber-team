331315665b021fc87db972f3d9c1906cf9c549ec
anotherbondan <anandagautama3112@gmail.com>
2026-04-02 14:15:31 +0700

    fix: resolve hydration miss at navbar

--- src/components/elements/Navbar/index.tsx
+++ src/components/elements/Navbar/index.tsx
@@ -3,45 +3,59 @@ import Link from "next/link";
 import { NAV_LINKS, NAV_LINKS_AUTH } from "./const";
 import { usePathname } from "next/navigation";
 import { authClient } from "../../../lib/auth-client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LogoutModal from "../../ui/logout-modal";
 
 export default function Navbar() {
   const path = usePathname();
   const { data: session } = authClient.useSession();
   const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
-  
+  const [mounted, setMounted] = useState(false);
+
+  // 1. Hydration fix: Ensure the component knows it has mounted on the client
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   const links = session ? NAV_LINKS : NAV_LINKS_AUTH;
 
   return (
     <nav className="flex justify-between items-center w-full py-4 px-14 fixed z-50">
       <Link href="/">
-        <h1 className="font-jetbrains font-extralight text-foreground">cyber-team</h1>
+        <h1 className="font-jetbrains font-extralight text-foreground">
+          cyber-team
+        </h1>
       </Link>
       <ul className="flex gap-4 font-jetbrains font-extralight text-foreground/50">
         {links.map((link) => {
           const isActive = link.href === path;
-          
+
           if (link.label === "sign-out") {
             return (
-              <span 
-                key={link.label} 
-                onClick={() => setIsLogoutModalOpen(true)} 
+              <span
+                key={link.label}
+                onClick={() => setIsLogoutModalOpen(true)}
                 className={`hover:text-foreground transition-colors hover:border-b-2 hover:cursor-pointer`}
               >
                 {link.label}
               </span>
-            )
+            );
           }
 
           return (
-            <Link href={link.href} key={link.href} className={`hover:text-foreground transition-colors hover:border-b-2 ${isActive ? "border-b-2 text-foreground" : ""}`}>
+            <Link
+              href={link.href}
+              key={link.href}
+              className={` transition-colors ${isActive && mounted ? "border-b-2 text-foreground" : "hover:text-foreground  hover:border-b-2"}`}
+            >
               {link.label}
             </Link>
-          )
+          );
         })}
       </ul>
-      {isLogoutModalOpen && <LogoutModal onClose={() => setIsLogoutModalOpen(false)} />}
+      {isLogoutModalOpen && (
+        <LogoutModal onClose={() => setIsLogoutModalOpen(false)} />
+      )}
     </nav>
   );
 }
